28f63581b796df7c39102941d41e62e7dfbf5eb0
Michael Karliner <mike@modern-industry.com>
2015-02-16 18:49:37 +0000

    CHANGE: add git to package.js, and bump

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'mkarliner:mosca',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Mosca is a node.js MQTT broker',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mkarliner/meteor-mosca',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Npm.depends({
-	"zmq": 2.10.0,
+	"zmq": "2.10.0",
 	"uglify-js": "2.4.16",
 	"mosca": "0.28.0"
 })
